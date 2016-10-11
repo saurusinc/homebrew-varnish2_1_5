291c0f61d55a0fa3444d17fcb87adc40ddb7df04
Joshua Fraser <joshua.fraser@gmail.com>
2016-10-10 17:34:06 -0700

    md5 is deprecated, not sure why this wasn't caught during local installation

--- varnish2_1_5.rb
+++ varnish2_1_5.rb
@@ -2,14 +2,9 @@ require 'formula'
 
 class Varnish2_1_5 < Formula
   url 'http://repo.varnish-cache.org/source/varnish-2.1.5.tar.gz'
-  head 'http://repo.varnish-cache.org/source/varnish-3.0.0-beta1.tar.gz'
   homepage 'http://www.varnish-cache.org/'
 
-  if ARGV.build_head?
-    md5 'c4dbd66ac6795c6c9d1c143ef2a47d38'
-  else
-    md5 '2d2f227da36a2a240c475304c717b8e3'
-  end
+  sha256 '2d8049be14ada035d0e3a54c2b519143af40d03d917763cf72d53d8188e5ef83'
 
   depends_on 'pkg-config' => :build
   depends_on 'pcre'
