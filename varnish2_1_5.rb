695033f1e8dc91bcc8c1107ab960338b331c5780
Joshua Fraser <joshua.fraser@gmail.com>
2016-10-10 17:20:29 -0700

    Initial commit - added Homebrew formula and updated README.md with pertinent information.

---
+++ varnish2_1_5.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+class Varnish2_1_5 < Formula
+  url 'http://repo.varnish-cache.org/source/varnish-2.1.5.tar.gz'
+  head 'http://repo.varnish-cache.org/source/varnish-3.0.0-beta1.tar.gz'
+  homepage 'http://www.varnish-cache.org/'
+
+  if ARGV.build_head?
+    md5 'c4dbd66ac6795c6c9d1c143ef2a47d38'
+  else
+    md5 '2d2f227da36a2a240c475304c717b8e3'
+  end
+
+  depends_on 'pkg-config' => :build
+  depends_on 'pcre'
+
+  # Do not strip varnish binaries: Otherwise, the magic string end pointer isn't found.
+  skip_clean :all
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--localstatedir=#{var}"
+    system "make install"
+    (var+'varnish').mkpath
+  end
+end
